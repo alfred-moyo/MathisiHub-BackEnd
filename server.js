4f0e5298f6795c56f55cf96f71640df95ebb1c11
alfred-moyo <alf.moxz@gmail.com>
2024-11-28 00:19:37 +0400

    update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // Load environment variables from properties file
 const properties = PropertiesReader('./dbconnection.properties');
 const dbPrefix = properties.get('db.prefix');
@@ -22,27 +21,24 @@ const dbParams = properties.get('db.params');
 // Construct MongoDB URI
 const dbURI = `${dbPrefix}${dbUser}:${dbPassword}@${dbHost}/${dbName}?${dbParams}`;
 
-
 // Initializing the middleware logger
 app.use((req, res, next) => {
     const time = new Date().toISOString();
     console.log(`${req.method} ${req.url} - ${time}`);
     next();
-})
+});
 
-// static file middleware
-
+// Static file middleware
 const imagePath = path.resolve(process.cwd(), 'images');
 app.use('/images', express.static(imagePath, { fallthrough: true }));
 
 // Handle 404 for missing images
 app.use('/images', (req, res) => {
     console.log(`Image not found: ${req.originalUrl}`);
-    // res.status(404).json({ error: 'Image not found' });
 });
 
-
-let db; // Database instance
+// Database instance
+let db;
 let client;
 
 // Connect to MongoDB
@@ -63,65 +59,64 @@ app.get('/', (req, res) => {
     res.send('Hi, Server is running!');
 });
 
+// Fetch all lessons
 app.get('/lessons', async (req, res) => {
-    try{
+    try {
         const lessons = await db.collection('lessons').find({}).toArray();
         res.json(lessons);
         console.log('Showing lessons');
     } catch (err) {
         console.error(err);
-        res.status(500).json({error: 'Failed to fetch lessons'});
+        res.status(500).json({ error: 'Failed to fetch lessons' });
     }
 });
 
-app.get('/search', async (req, res) => {
-    try {
-        const searchQuery = req.query.term; 
-        if (!searchQuery) {
-            return res.status(400).json({ error: "Search query is required" });
-        }
-
-        const database = client.db('Xkool-eShop');
-        const collection = database.collection('Programs');
-
-        let query = {};
-        
-
-        if (!isNaN(searchQuery)) {
-            const numberAsString = searchQuery.toString();
-            query = {
-                $or: [
-                    { $expr: { $regexMatch: {input: { $toString: "$price" }, regex: numberAsString, options: "i" } } }, 
-                    { $expr: { $regexMatch: {input: { $toString: "$availableSpaces" }, regex: numberAsString, options: "i" } } }
-                ]
-            };
-        } else {
-            query = {
-                $or: [
-                    { title: { $regex: searchQuery, $options: 'i' } }, 
-                    { location: { $regex: searchQuery, $options: 'i' } }                 
-                ]
-            };
-        }
-
-        const results = await collection.find(query).toArray();
-
-        res.json(results);
-        console.log(`Search successful with query: "${searchQuery}"`);
-    } catch (error) {
-        res.status(500).json({ error: "Search Failed!" });
-        console.error("Error performing search:", error);
-    }
-});
-
+// Create a new order
 app.post('/order', async (req, res) => {
-    try{
+    try {
         const order = req.body;
         const result = await db.collection('orders').insertOne(order);
         res.json(result);
         console.log('Order created');
     } catch (err) {
         console.error(err);
+    }
+});
+
+// Search endpoint
+app.get('/search', async (req, res) => {
+    try {
+        const searchQuery = req.query.term;
+        if (!searchQuery) {
+            return res.status(400).json({ error: "Search query is required" });
+        }
+
+        const database = client.db('Mathisi-Hub');
+        const collection = database.collection('lessons');
+
+        let query = {};
+
+        if (!isNaN(searchQuery)) {
+            const numberAsString = searchQuery.toString();
+            query = {
+                $or: [
+                    { $expr: { $regexMatch: { input: { $toString: "$price" }, regex: numberAsString, options: "i" } } }
+                ]
+            };
+        } else {
+            query = {
+                $or: [
+                    { title: { $regex: searchQuery, $options: 'i' } }
+                ]
+            };
+        }
+
+        const results = await collection.find(query).toArray();
+        res.json(results);
+        console.log(`Search successful with query: "${searchQuery}"`);
+    } catch (error) {
+        res.status(500).json({ error: "Search Failed!" });
+        console.error("Error performing search:", error);
     }
 });
 
